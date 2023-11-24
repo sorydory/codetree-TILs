ab9f8c336a65e648dd61e080a7b809f90799699e
Jin Sol Jeon <116371230+sorydory@users.noreply.github.com>
2023-11-24 17:47:58 +0900

    Title: 합과 평균, Time: 294ms, Memory: 12MB, Status: Passed - CodeTree

--- 231124/합과 평균/sum-and-avg.js
+++ 231124/합과 평균/sum-and-avg.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().split(" ");
+let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
-
-console.log(a+b,(a+b)/2);
+let average = (a+b)/2;
+console.log(a+b,(average.toFixed(1)));
