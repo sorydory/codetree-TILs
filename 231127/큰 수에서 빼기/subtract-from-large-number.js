d79ebe5bd8dc20c1207aa49d6f5230675959bc2a
Jin Sol Jeon <116371230+sorydory@users.noreply.github.com>
2023-11-27 10:44:11 +0900

    Title: 큰 수에서 빼기, Time: 273ms, Memory: 13MB, Status: Passed - CodeTree

--- 231127/큰 수에서 빼기/subtract-from-large-number.js
+++ 231127/큰 수에서 빼기/subtract-from-large-number.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
-if(a>b) {
+if(a>=b) {
     console.log(a-b);
 }
 
