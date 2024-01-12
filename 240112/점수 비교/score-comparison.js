025ab0133d47740c9d82d822ab932a9d5893b1a1
sorydory <116371230+sorydory@users.noreply.github.com>
2024-01-12 17:04:12 +0900

    Title: 점수 비교, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240112/점수 비교/score-comparison.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let arrA = input[0].split(" ");
+let arrB = input[1].split(" ");
+
+let mathA = Number(arrA[0]), engA = Number(arrA[1]);
+let mathB = Number(arrB[0]), engB = Number(arrB[1]);
+
+
+if (mathA > mathB && engA > engB) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
